906d756bee38cd6e9ad4f90d9b5b26a5ba6b3e4c
oana-iu <oana.mitri01@gmail.com>
2024-05-31 09:35:25 +0300

    add notFound route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
 require("dotenv").config();
 
 //middlewares
@@ -12,6 +13,7 @@ app.use(express.json());
 //routes
 
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
 
 const start = async () => {
     try {
