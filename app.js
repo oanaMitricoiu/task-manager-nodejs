7856f8fa13a0b815d58b194fb4b6b45b2abd12ca
oana-iu <oana.mitri01@gmail.com>
2024-05-31 10:30:23 +0300

    add error middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
 //middlewares
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
     try {
